4ab8c09ef60505b5297174c83a960e9b8b99f274
Jason Lane & Lindsay Culver <student@epicodus.com>
2015-08-04 13:34:43 -0700

    Add hash scrabble score tests

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -10,4 +10,14 @@ describe String do
   it 'returns a scrabble score for a word' do
     expect('Hi'.scrabble_score).to eq(5)
   end
+
+  it("returns a scrabble score for a letter from a hash") do
+    expect("a".hash_scrabble_score()).to(eq(1))
+  end
+
+  it 'returns a scrabble score for a word from a hash' do
+    expect('Hi'.hash_scrabble_score).to eq(5)
+  end
+
+
 end
